623adfb3315c541f827292b7579f0ec574f9cf9e
David Vargas <dvargas@mit.edu>
2017-10-06 13:05:47 -0400

    hidden semantic; public code gen

--- tests/testall.sh
+++ tests/testall.sh
@@ -1,10 +1,41 @@
 #!/bin/bash
-
-echo "Scanner Tests";
-./scanner/test.sh;
-echo "Parser Tests";
-./parser/test.sh
-echo "Hidden Scanner Tests";
-./scanner-hidden/test.sh;
-echo "Hidden Parser Tests";
-./parser-hidden/test.sh
+OS="`uname`"
+ROOT="$(git rev-parse --show-toplevel)/tests";
+case $OS in
+  Linux*|Darwin*)
+    echo "Scanner Tests";
+    $ROOT/scanner/test.sh;
+    echo "Parser Tests";
+    $ROOT/parser/test.sh;
+    echo "Hidden Scanner Tests";
+    $ROOT/scanner-hidden/test.sh;
+    echo "Hidden Parser Tests";
+    $ROOT/parser-hidden/test.sh;
+    echo "Semantic Tests";
+    $ROOT/semantics/test.sh;
+    echo "Semantic Hidden Tests";
+    $ROOT/semantics-hidden/test.sh;
+    echo "Codegen Tests";
+    $ROOT/codegen/test.sh;
+    ;;
+  CYGWIN*|Windows*)
+    cd $ROOT;
+    echo "Scanner Tests";
+    bash scanner/test.sh;
+    echo "Parser Tests";
+    bash parser/test.sh;
+    echo "Hidden Scanner Tests";
+    bash scanner-hidden/test.sh;
+    echo "Hidden Parser Tests";
+    bash parser-hidden/test.sh;
+    echo "Semantic Tests";
+    bash semantics/test.sh;
+    echo "Semantic Tests";
+    bash semantics-hidden/test.sh;
+    echo "Semantic Tests";
+    bash codegen/test.sh;
+    ;;
+  *)
+    echo "Error: Unknown $OS";
+    ;;
+esac
