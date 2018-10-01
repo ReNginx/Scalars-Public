abe57a297155a8e745d88725b984a71d55e17735
Jack <jchoi5@me.com>
2018-10-01 17:47:56 -0400

    Rough draft of Descriptors.scala

--- src/ir/Descriptors.scala
+++ src/ir/Descriptors.scala
@@ -1,20 +1,54 @@
 package ir
 
+import scala.collection.immutable.HashMap
 import scala.collection.mutable.ListBuffer
 
-case class ClassDescriptor (
-  parentClass: ClassDescriptor,
-  fields: SymbolTable,
-  methods: SymbolTable
-)
+trait Descriptor {
+  def name: String
+  def line: Int
+  def column: Int
+  // stack offset
+  // memory address
+}
 
-case class MethodDescriptor (
-  code: ScalarAST,
-  localVariables: SymbolTable,
-  parameters: SymbolTable
-)
+case class ClassDescriptor(
+  name: String,
+  line: Int,
+  column: Int,
+  methods: SymbolTable[MethodDescriptor]
+) extends Descriptor
 
-// local, parameter, field
-case class VariableDescriptor ()
+case class ParameterDescriptor(
+  name: String,
+  line: Int,
+  column: Int,
+  types: String
+) extends Descriptor
 
-case class TypeDescriptor ()
+case class VariableDescriptor(
+  name: String,
+  line: Int,
+  column: Int,
+  types: String,
+  value: String
+) extends Descriptor
+
+class MethodDescriptor(
+  methodName: String,
+  line: Int,
+  column: Int,
+  types: HashMap[Vector[ParameterDescriptor], String],
+  codes: HashMap[Vector[ParameterDescriptor], ScalarAST]
+) extends Descriptor {
+
+  private val _name = methodName
+
+  def name = _name
+
+  // get the reutrn type corresponding to the given parameters
+  def returnType(params: Vector[ParameterDescriptor]): String = throw RuntimeException()
+
+  // get the ScalarAST corresponding to the given parameters
+  def code(params: Vector[ParameterDescriptor]): ScalarAST = throw RuntimeException()
+
+}
