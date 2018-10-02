709f9018cdf45702257301ae7a9f464fc1e31a7c
Jack <jchoi5@me.com>
2018-10-01 20:26:37 -0400

    Descriptors roughly sketched

--- src/ir/Descriptors.scala
+++ src/ir/Descriptors.scala
@@ -3,40 +3,49 @@ package ir
 import scala.collection.immutable.HashMap
 import scala.collection.mutable.ListBuffer
 
+import edu.mit.compilers.grammar.DecafParserTokenTypes
+
 trait Descriptor {
   def name: String
   def line: Int
   def column: Int
-  // stack offset
-  // memory address
 }
 
-case class ClassDescriptor(
+case class ProgramDescriptor(
   name: String,
   line: Int,
   column: Int,
+  fields:  SymbolTable[VariableDescriptor],
   methods: SymbolTable[MethodDescriptor]
-) extends Descriptor
-
-case class ParameterDescriptor(
-  name: String,
-  line: Int,
-  column: Int,
-  types: String
-) extends Descriptor
-
-case class VariableDescriptor(
-  name: String,
-  line: Int,
-  column: Int,
-  types: String,
-  value: String
 ) extends Descriptor
 
 case class MethodDescriptor(
   name: String,
   line: Int,
   column: Int,
-  types: HashMap[Vector[ParameterDescriptor], String],
-  codes: HashMap[Vector[ParameterDescriptor], ScalarAST]
+  variables:   HashMap[Vector[ParameterDescriptor], SymbolTable[VariableDescriptor]],
+  returnTypes: HashMap[Vector[ParameterDescriptor], String],
+  codes:       HashMap[Vector[ParameterDescriptor], ScalarAST]
+) extends Descriptor
+
+case class ParameterDescriptor(
+  name: String,
+  line: Int,
+  column: Int,
+  paramType: DecafParserTokenTypes  // either int or bool
+) extends Descriptor
+
+case class VariableDescriptor(
+  name: String,
+  line: Int,
+  column: Int,
+  varType: DecafParserTokenTypes,  // either int or bool
+  value: String
+) extends Descriptor
+
+case class BooleanDescriptor(
+  name: String,
+  line: Int,
+  column: Int,
+  value: String
 ) extends Descriptor
