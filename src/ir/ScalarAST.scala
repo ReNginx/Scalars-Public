8a9868f2fd6c31dfe5dbac20e8229c7ff8462725
Jack <jchoi5@me.com>
2018-10-01 21:09:00 -0400

    ScalarAST code cleaned

--- src/ir/ScalarAST.scala
+++ src/ir/ScalarAST.scala
@@ -3,20 +3,27 @@ package ir
 import scala.collection.mutable.ListBuffer
 
 import antlr.CommonAST
-
 import edu.mit.compilers.grammar.DecafParserTokenTypes
 
-/** Immutable, recursive AST with simpler APIs than CommonAST.
+/** Immutable, recursive ADT with simpler APIs than CommonAST.
  *
  * Don't use the constructor of this class directly.
  * Use ScalarAST.fromCommonAST
  *
- * This class offers the following that CommonAST does not:
- *   1. child nodes as a collection
- *   2. pointer to the parent node
- *   3. pretty-printing
+ * This class differs from CommonAST in the following ways:
+ *   1. each node contains children nodes as a collection
+ *   2. each node contains pointer to the parent node
+ *   3. has pretty-printing 
  */
-case class ScalarAST(token: Int, text: String, line: Int, column: Int, parent: Option[ScalarAST])(lazyChildren: => Vector[ScalarAST]) {
+case class ScalarAST(
+  token: Int,
+  text: String,
+  line: Int,
+  column: Int,
+  parent: Option[ScalarAST]
+)(
+  lazyChildren: => Vector[ScalarAST]
+) {
 
   def children = lazyChildren
 
@@ -25,17 +32,16 @@ case class ScalarAST(token: Int, text: String, line: Int, column: Int, parent: O
    * @param numSpace OPTIONAL number of space to represent a single indent
    */
   def prettyPrint(indentLevel: Int = 0, numSpace: Int = 2): Unit = {
-    // `indentLevel * numSpace` number of spaces
     val leadingWS = " " * numSpace * indentLevel
 
     // if line & column are both 0, this token is virtual and doesn't actually exist in source code
     val location = if (line == 0 && column == 0) "" else s"  (${line}:${column})"
     println(s"${leadingWS}${token}:${text}${location}")
 
-    // recurse
+    // recurse for each child
     children foreach { _.prettyPrint(indentLevel + 1) }
   }
-  
+
   /**
   def printself(): Unit = {
     println(this)
@@ -49,17 +55,16 @@ case class ScalarAST(token: Int, text: String, line: Int, column: Int, parent: O
 
 object ScalarAST {
 
-  /** Get all children of `ast` as a Vector.
+  /** Get all children nodes as a Vector.
    *
-   * @param ast
-   * @return all children of `ast` as a Vector, with order of children preserved
+   * @param parent the parent node whose children will be returned
+   * @return all children of `parent` as a Vector, with the order of children preserved
    */
-  private def getChildren(ast: CommonASTWithLines): Vector[CommonASTWithLines] = {
-    // all children of `ast`
+  private def getChildren(parent: CommonASTWithLines): Vector[CommonASTWithLines] = {
     val children = ListBuffer[CommonASTWithLines]()
 
     // aggregate all children
-    var childOpt = Option(ast.getFirstChild)
+    var childOpt = Option(parent.getFirstChild)
     while (! childOpt.isEmpty) {
       val child = childOpt.get
       children += child
@@ -78,33 +83,36 @@ object ScalarAST {
    */
   def fromCommonAST(ast: CommonASTWithLines, parent: Option[ScalarAST] = None): ScalarAST = {
     var commonChildren = getChildren(ast)
-    val line = ast.getLine
     val column = ast.getColumn
-    val token = ast.getType
+    val line = ast.getLine
     val text = ast.getText
-    
+    val token = ast.getType
+
+    // leaf node, so no children
     if (commonChildren.size == 0) {
-      new ScalarAST(token, text, line, column, parent)({ Vector() })
-    } else {
-      lazy val thisAST: ScalarAST = new ScalarAST(token, text, line, column, parent)({ children })
-
-      lazy val children = {
-        val current = commonChildren map (child => fromCommonAST(child, Option(thisAST)))
-        if (token == DecafParserTokenTypes.METHOD_CALL) {  // make sure args is present, even if empty
-          val token = DecafParserTokenTypes.ARGS
-          val name = "ARGS"
-          val args = current map { _.text } filter { _ == name }
-          val emptyArgs = new ScalarAST(token, name, 0, 0, Option(thisAST))({ Vector() })
-          current ++ {
-            if (args.size == 0) Vector(emptyArgs) else Vector()
-          }
-        } else {
-          current
-        }
-      }
-
-      thisAST
+      return new ScalarAST(token, text, line, column, parent)({ Vector() })
     }
+
+    // lazy eval to create immutable double-link
+    lazy val thisNode: ScalarAST = new ScalarAST(token, text, line, column, parent)({ children })
+    lazy val children = {
+      val thisASTOpt = Option(thisNode)
+      val originalChildren = commonChildren map { fromCommonAST(_, thisASTOpt) }
+
+      // make sure method call has argument list, even if it is empty
+      if (token == DecafParserTokenTypes.METHOD_CALL) {
+        val name = "ARGS"
+        val args = originalChildren map { _.text } filter { _ == name }
+
+        // add empty args vector if there isn't currently one
+        val emptyArgs = new ScalarAST(DecafParserTokenTypes.ARGS, name, 0, 0, thisASTOpt)({ Vector() })
+        originalChildren ++ { if (args.size == 0) Vector(emptyArgs) else Vector() }
+      } else {
+        originalChildren
+      }
+    }
+
+    thisNode
   }
 
 }
