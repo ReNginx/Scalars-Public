262f48d1b20344aa51ed61814f133495725a9762
Allen Mi <allen.mi@yale.edu>
2018-10-20 19:51:07 -0400

    Remove legacy files

--- src/ir/typed/MethodDeclaration.scala
+++
@@ -1,5 +0,0 @@
-package ir.typed
-
-case class MethodDeclaration(line: Int, col: Int, name: String, typ: Type, params: Vector[FieldDeclaration], block: Block) extends MemberDeclaration {
-  override def toString: String = s"MethodDeclaration ${typ} ${name} ${line}:${col}"
-}
