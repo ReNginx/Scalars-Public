e8ef69df3f8875121e6c1068b11e866c762782cf
Allen Mi <allen.mi@yale.edu>
2018-10-20 20:26:02 -0400

    Re-add tests
    Re-add tests

--- tests/scanner-hidden/test.sh
+++ tests/scanner-hidden/test.sh
@@ -1,31 +1,5 @@
 #!/bin/sh
 
-# runscanner() {
-#     curdir=$PWD
-#     cd `dirname $1`
-#     $(git rev-parse --show-toplevel)/run.sh -t scan `basename $1`
-#     cd $curdir
-# }
-#
-# exitcode=0
-# fail=0
-# count=0
-#
-# for file in `dirname $0`/input/*; do
-#   output=`tempfile`
-#   runscanner $file > $output 2>&1;
-#   if ! diff -u $output `dirname $0`/output/`basename $file`.out; then
-#     echo "File $file scanner output mismatch.";
-#     exitcode=1
-#     fail=$((fail+1))
-#   fi
-#   count=$((count+1))
-#   rm $output;
-# done
-#
-# echo "Failed $fail tests out of $count";
-# exit $exitcode;
-
 runscanner() {
     curdir=$PWD
     cd `dirname $1`
@@ -37,23 +11,16 @@ exitcode=0
 fail=0
 count=0
 
-mkdir "$(dirname $0)/actual_output"
 for file in `dirname $0`/input/*; do
-  if [ -f "$(dirname $0)/input/$1" ] && [ "$(basename $file)" != "$1" ]; then
-    continue
-  fi
-
-  echo
-  output=$(tempfile 2> /dev/null || mktemp)
+  output=`tempfile`
   runscanner $file > $output 2>&1;
-  if ! colordiff $output "$(dirname $0)/output/$(basename $file).out"; then
+  if ! diff -u $output `dirname $0`/output/`basename $file`.out; then
     echo "File $file scanner output mismatch.";
     exitcode=1
     fail=$((fail+1))
   fi
   count=$((count+1))
-  cat $output > "$(dirname $0)/actual_output/$(basename $file)"
-  rm -f $output;
+  rm $output;
 done
 
 echo "Failed $fail tests out of $count";
