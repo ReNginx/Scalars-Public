150c266b0aa66be52e2c8ca818f334ab6bcb0289
HanxRen <969765303@qq.com>
2018-10-27 21:03:32 -0400

    modfied CFG setting, comment out old code

--- src/codegen/Allocate.scala
+++ src/codegen/Allocate.scala
@@ -1,78 +1,78 @@
-package codegen
-
-import scala.collection.mutable.{HashSet, Set}
-import scala.collection.immutable.Map
-
-import ir.components._
-import ir.PrettyPrint
-
-object Allocate {
-  val sizeOfVar:Int = 8
-  var offset:Int = 0
-
-  def apply(cfg: CFG): Unit = {
-    if (cfg.isAllocated)
-      return
-    cfg.isAllocated = true
-
-    cfg match {
-      case VirtualCFG(_, _, next) => {
-        if (next.isDefined)
-          Allocate(next.get)
-      }
-
-      case CFGBlock(_, statements, next, _) => {
-        for (statement <- statements) {
-          statement match {
-            case variable: VariableDeclaration => {
-                variable.offset = offset
-                offset -= sizeOfVar
-            }
-
-            case array: ArrayDeclaration => {
-              array.offset = offset
-              offset -= array.length.value.toInt * sizeOfVar
-            }
-
-            case _ =>
-          }
-        }
-
-        if (next.isDefined)
-            Allocate(next.get)
-      }
-
-      case CFGConditional(_, _, _, next, ifFalse, _) => {
-        if (next.isDefined)
-          Allocate(next.get)
-        if (ifFalse.isDefined)
-          Allocate(ifFalse.get)
-      }
-
-      case CFGMethod(_, block, params, _, _, _, _) => {
-        for (param <- params) {
-          param.asInstanceOf[VariableDeclaration].offset = offset
-          offset -= sizeOfVar
-        }
-
-        Allocate(block.get)
-      }
-
-      case CFGMethodCall(_, _, _, next, _) => {
-        if (next.isDefined)
-          Allocate(next.get)
-      }
-
-      case CFGProgram(_, _, fields, methods, _, _) => {
-        Allocate(fields)
-        for ((string,method) <- methods) {
-          offset = -sizeOfVar // local var starts from -8
-          Allocate(method)
-          method.spaceAllocated = offset + sizeOfVar
-        }
-      }
-
-      case _ =>
-    }
-  }
-}
+//package codegen
+//
+//import scala.collection.mutable.{HashSet, Set}
+//import scala.collection.immutable.Map
+//
+//import ir.components._
+//import ir.PrettyPrint
+//
+//object Allocate {
+//  val sizeOfVar:Int = 8
+//  var offset:Int = 0
+//
+//  def apply(cfg: CFG): Unit = {
+//    if (cfg.isAllocated)
+//      return
+//    cfg.isAllocated = true
+//
+//    cfg match {
+//      case VirtualCFG(_, _, next) => {
+//        if (next.isDefined)
+//          Allocate(next.get)
+//      }
+//
+//      case CFGBlock(_, statements, next, _) => {
+//        for (statement <- statements) {
+//          statement match {
+//            case variable: VariableDeclaration => {
+//                variable.offset = offset
+//                offset -= sizeOfVar
+//            }
+//
+//            case array: ArrayDeclaration => {
+//              array.offset = offset
+//              offset -= array.length.value.toInt * sizeOfVar
+//            }
+//
+//            case _ =>
+//          }
+//        }
+//
+//        if (next.isDefined)
+//            Allocate(next.get)
+//      }
+//
+//      case CFGConditional(_, _, _, next, ifFalse, _) => {
+//        if (next.isDefined)
+//          Allocate(next.get)
+//        if (ifFalse.isDefined)
+//          Allocate(ifFalse.get)
+//      }
+//
+//      case CFGMethod(_, block, params, _, _, _, _) => {
+//        for (param <- params) {
+//          param.asInstanceOf[VariableDeclaration].offset = offset
+//          offset -= sizeOfVar
+//        }
+//
+//        Allocate(block.get)
+//      }
+//
+//      case CFGMethodCall(_, _, _, next, _) => {
+//        if (next.isDefined)
+//          Allocate(next.get)
+//      }
+//
+//      case CFGProgram(_, _, fields, methods, _, _) => {
+//        Allocate(fields)
+//        for ((string,method) <- methods) {
+//          offset = -sizeOfVar // local var starts from -8
+//          Allocate(method)
+//          method.spaceAllocated = offset + sizeOfVar
+//        }
+//      }
+//
+//      case _ =>
+//    }
+//  }
+//}
--- src/codegen/CFG.scala
+++ src/codegen/CFG.scala
@@ -40,7 +40,9 @@ case class VirtualCFG(
  * to the next block, represented by `next`.
  *
  * @param label a string that uniquely identifies this block
- * @param statements
+ * @param statements flattened 3addr code. should only be one of the following
+  *                   1. 3addr assignment which only contains arithmetic operator
+  *                   2. return
  * @param parent all possible blocks that the program could have been in, prior to this block
  * @param next the next block to go to, after all statements in this block has executed
  */
@@ -57,15 +59,14 @@ case class CFGBlock(
  * basic block becomes `ifTrue`, and `ifFalse` otherwise.
  *
  * @param label a string that uniquely identifies this block
- * @param statements the conditional statemnt that will determine where this block goes to next
+ * @param condition a simple logical expression that does not contains "&&" or "||"
  * @param parent all possible blocks that the program could have been in, prior to this block
  * @param next the next block to go to, if conditional evaluates to true
  * @param ifFalse the next block to go to, if conditional evaluates to false
  */
 case class CFGConditional(
     label: String,
-    // statements: Vector[IR],
-    condition: Location,
+    condition: Expression,
     parents: Set[CFG]=Set(),
     var next: Option[CFG] = None,
     var ifFalse: Option[CFG] = None,
@@ -75,7 +76,7 @@ case class CFGConditional(
  *
  * @param label a string that uniquely identifies this block
  * @param block control flow graph block, corresponding to the body of the method
- * @param params parameters of this method
+ * @param params parameters of this method, must be a declaration
  * @param next not used
  * @param parents the basic block where this method was declared
  * @param typ return type
@@ -83,28 +84,36 @@ case class CFGConditional(
 case class CFGMethod(
     label: String,
     var block: Option[CFG],  // for preventing recursion
-    params: Vector[IR],
-    method: LocMethodDeclaration,
+    params: Vector[VariableDeclaration],
+    method: MethodDeclaration,
     var next: Option[CFG] = None,  // no meaning
     parents: Set[CFG] = Set(),
     typ: Option[Type] = None) extends CFG { // where declared
       var spaceAllocated: Int = 0;
     }
 
+/**
+  *
+  * @param label a string that uniquely identifies this block
+  * @param params could either be a location or a literal
+  * @param declaration points to where it's defined
+  * @param next points to CFG after the call
+  * @param parents the basic block where this method was declared
+  */
 // `next` simply points to
+// where IR can be either Literal or Location
 case class CFGMethodCall(
     label: String,
-    params: Vector[IR],   // TODO Vector[IR], where IR can be either Literal or Location
+    params: Vector[Expression],
     declaration: CFGMethod,
-    var next: Option[CFG] = None,  // points to start of declration
-    parents: Set[CFG] = Set()) extends CFG  // where it was called
+    var next: Option[CFG] = None,
+    parents: Set[CFG] = Set()) extends CFG
 
 /** Basic Block in Control Flow Graph, which represents a program.
  */
 case class CFGProgram(
   label: String,
-  imports: Vector[IR],
   fields: CFGBlock,
-  methods: Map[String, CFGMethod],  // TODO change to vector
+  methods: Vector[CFGMethod],
   var next: Option[CFG] = None,  // not used
   parents: Set[CFG] = Set()) extends CFG  // not used
