ba529ca15ef3b047ef7adb41bf79a723feefce98
HanxRen <969765303@qq.com>
2018-11-12 16:02:17 -0500

    revert mis-modfied run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
+## Run script for Scalars Decaf Compiler
+
+if [[ $(dnsdomainname) = mit.edu ]]; then
+  eval $(attach -Padd -b -f scala)
+fi
+
 gitroot=$(git rev-parse --show-toplevel)
+. $gitroot/setenv.sh
+
 if [ -n "$JAVA_TOOL_OPTIONS" ]; then
   export JAVA_OPTS="$JAVA_TOOL_OPTIONS"
   unset JAVA_TOOL_OPTIONS
 fi
+
 scala -classpath $gitroot/build/lib/project.jar:$gitroot/lib/antlr.jar compile.Compiler "$@"
+
