a2e9946e3e46890d8514278a752b32b87ffe7a4e
renginx <969765303@qq.com>
2018-11-17 16:02:50 -0500

    a untested version of constantfolding.

--- src/optimization/ConstantFolding.scala
+++ src/optimization/ConstantFolding.scala
@@ -2,6 +2,7 @@
 //
 //import codegen.{CFG, CFGConditional, CFGMethod, CFGProgram}
 //import ir.components._
+//import jdk.nashorn.internal.runtime.options.LoggingOption.LoggerInfo
 //
 //object ConstantFolding extends Optimization {
 //
@@ -34,35 +35,65 @@
 //      }
 //
 //      case block: Block => {
-//        for (stmt <- block.statements) {
+//        for (idx <- block.statements.indices) {
+//          val stmt = block.statements(idx)
 //          stmt match {
-//            case assign: AssignStatement => {
-//
-//            }
-//            case compoundAsg: CompoundAssignStatement => {
-//
-//            }
-//            case inc: Increment => {
-//
-//            }
-//            case dec: Decrement => {
-//
-//            }
-//
 //            case unary: UnaryOperation => {
 //              unary.expression match {
-//                case IntLiteral =>
+//                case lit: IntLiteral => {
+//                  val calculatedValue = unary match {
+//                    case Not => IntLiteral(lit.line, lit.col, ~lit.value)
+//                    case Negate => IntLiteral(lit.line, lit.col, -lit.value)
+//                  }
+//                  block.statements(idx) = AssignStatement(unary.line, unary.col, unary.eval.get, calculatedValue)
+//                }
 //                case _ =>
 //              }
 //            }
 //
 //            case binary: BinaryOperation => {
+//              if (binary.lhs.isInstanceOf[Literal] && binary.rhs.isInstanceOf[Literal]) {
+//                lazy val lhsBool = binary.lhs.asInstanceOf[BoolLiteral]
+//                lazy val rhsBool = binary.rhs.asInstanceOf[BoolLiteral]
+//                lazy val lhsInt = binary.lhs.asInstanceOf[IntLiteral]
+//                lazy val rhsInt = binary.rhs.asInstanceOf[IntLiteral]
 //
+//                val calculatedValue = binary match {
+//                  case logical: LogicalOperation =>
+//                    logical.operator match {
+//                      case And => BoolLiteral(binary.line, binary.col, lhsBool.value && rhsBool.value)
+//                      case Or => BoolLiteral(binary.line, binary.col, lhsBool.value || rhsBool.value)
+//                      case Equal => binary.lhs match {
+//                        case _: BoolLiteral => BoolLiteral(binary.line, binary.col, lhsBool.value == rhsBool.value)
+//                        case _: IntLiteral => BoolLiteral(binary.line, binary.col, lhsInt.value == rhsInt.value)
+//                      }
+//                      case NotEqual => binary.lhs match {
+//                        case _: BoolLiteral => BoolLiteral(binary.line, binary.col, lhsBool.value == rhsBool.value)
+//                        case _: IntLiteral => BoolLiteral(binary.line, binary.col, lhsInt.value == rhsInt.value)
+//                      }
+//                      case GreaterThan => BoolLiteral(binary.line, binary.col, lhsInt.value > rhsInt.value)
+//                      case GreaterThanOrEqual => BoolLiteral(binary.line, binary.col, lhsInt.value >= rhsInt.value)
+//                      case LessThan => BoolLiteral(binary.line, binary.col, lhsInt.value < rhsInt.value)
+//                      case LessThanOrEqual => BoolLiteral(binary.line, binary.col, lhsInt.value <= rhsInt.value)
+//                    }
+//                  case arith: ArithmeticOperation =>
+//                    arith.operator match {
+//                      case Add => IntLiteral(binary.line, binary.col, lhsInt.value + rhsInt.value)
+//                      case Divide => IntLiteral(binary.line, binary.col, lhsInt.value / rhsInt.value)
+//                      case Modulo => IntLiteral(binary.line, binary.col, lhsInt.value % rhsInt.value)
+//                      case Multiply => IntLiteral(binary.line, binary.col, lhsInt.value * rhsInt.value)
+//                      case Subtract => IntLiteral(binary.line, binary.col, lhsInt.value - rhsInt.value)
+//                    }
+//                }
+//
+//                block.statements(idx) = AssignStatement(binary.line, binary.col, binary.eval.get, calculatedValue)
+//              }
 //            }
 //
 //            case _ =>
 //          }
 //        }
+//
 //        if (block.next.isDefined) {
 //          ConstantFolding(block.next.get)
 //        }
