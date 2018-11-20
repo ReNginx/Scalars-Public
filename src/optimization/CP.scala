a7695f8a2fd77eb33a4144356cbbf6833a5a9581
Allen Mi <allen.mi@yale.edu>
2018-11-19 22:35:25 -0500

    Recommit CP fix

--- src/optimization/CP.scala
+++ src/optimization/CP.scala
@@ -7,8 +7,16 @@ import codegen._
 object CP extends Optimization {
 
   val localTmpSuffix = "_cse_local_tmp"
+  val tmp2Var: Map[Location, SingleExpr] = Map[Location, SingleExpr]()
+  val var2Set: Map[SingleExpr, Set[Location]] = Map[SingleExpr, Set[Location]]()
  
+  private def init(): Unit = {
+    tmp2Var.clear
+    var2Set.clear
+  }
+  
   def apply(cfg: CFG): Unit = {
+    init
     if (!cfg.isOptimized(CP)) {
       cfg.setOptimized(CP)
       cfg match {
@@ -24,27 +32,27 @@ object CP extends Optimization {
             statement match {
               case assign: AssignStatement => { // only optimize for AssignStatement
                 if (assign.loc.name.endsWith(localTmpSuffix)) { // t1 = a
-                  block.tmp2Var += (assign.loc -> assign.value.asInstanceOf[SingleExpr]) // add t1 -> a
-                  if (block.var2Set.contains(assign.value.asInstanceOf[SingleExpr])) { // add a -> {t1}
-                    block.var2Set(assign.value.asInstanceOf[SingleExpr]) += assign.loc
+                  tmp2Var += (assign.loc -> assign.value.asInstanceOf[SingleExpr]) // add t1 -> a
+                  if (var2Set.contains(assign.value.asInstanceOf[SingleExpr])) { // add a -> {t1}
+                    var2Set(assign.value.asInstanceOf[SingleExpr]) += assign.loc
                   } else {
-                    block.var2Set += (assign.value.asInstanceOf[SingleExpr] -> Set[Location](assign.loc))
+                    var2Set += (assign.value.asInstanceOf[SingleExpr] -> Set[Location](assign.loc))
                   }
                   newStatements += statement
                 } else {
                   assign.value match {
                     case rhsLoc: Location => {
                       if (rhsLoc.name.endsWith(localTmpSuffix)) { // c = t1
-                        if (block.tmp2Var.contains(rhsLoc)) { // c = t1 => c = a
-                          val replaceStatement: AssignStatement = AssignStatement(assign.line,  assign.col, assign.loc, block.tmp2Var(rhsLoc))
+                        if (tmp2Var.contains(rhsLoc)) { // c = t1 => c = a
+                          val replaceStatement: AssignStatement = AssignStatement(assign.line,  assign.col, assign.loc, tmp2Var(rhsLoc))
                           newStatements += replaceStatement
                         } else {
                           throw new NotImplementedError // should be impossible
                         }
-                        if (block.var2Set.contains(assign.loc)) { // remove c from tmp2Var and var2Set
-                          val lhsSet: Set[Location] = block.var2Set(assign.loc)
-                          lhsSet foreach { block.tmp2Var.remove(_) }
-                          block.var2Set.remove(assign.loc)
+                        if (var2Set.contains(assign.loc)) { // remove c from tmp2Var and var2Set
+                          val lhsSet: Set[Location] = var2Set(assign.loc)
+                          lhsSet foreach { tmp2Var.remove(_) }
+                          var2Set.remove(assign.loc)
                         }
                       } else {
                         newStatements += statement
@@ -54,10 +62,10 @@ object CP extends Optimization {
                       newStatements += statement
                     }
                   }
-                  if (block.var2Set.contains(assign.loc)) { // remove c from tmp2Var and var2Set
-                    val lhsSet: Set[Location] = block.var2Set(assign.loc)
-                    lhsSet foreach { block.tmp2Var.remove(_) }
-                    block.var2Set.remove(assign.loc)
+                  if (var2Set.contains(assign.loc)) { // remove c from tmp2Var and var2Set
+                    val lhsSet: Set[Location] = var2Set(assign.loc)
+                    lhsSet foreach { tmp2Var.remove(_) }
+                    var2Set.remove(assign.loc)
                   }
                 }
               }
