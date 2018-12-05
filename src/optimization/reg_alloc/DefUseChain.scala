7dcc85fdb10ff4a0317929552707b2f3dcb11769
HanxRen <969765303@qq.com>
2018-12-04 22:18:43 -0500

    draft of DU chain

---
+++ src/optimization/reg_alloc/DefUseChain.scala
@@ -0,0 +1,20 @@
+package optimization.reg_alloc
+
+import ir.components.Location
+import optimization.Labeling.StmtId
+
+/**
+  * StmtId is a tuple of (CFG, Int)
+  * if CFG is CFGBLOCK, Int is the index of statement in block.statement
+  * otherwise, Int is always -1.
+  * @param DefPos
+  * @param UsePos
+  * @param DefLoc
+  * @param UseLoc
+  */
+case class DefUseChain(DefPos:StmtId,
+                       UsePos:StmtId,
+                       DefLoc:Location,
+                       UseLoc:Location) {
+
+}
