49095ef1fc1376e49167b7d485e822dd51f34fc4
Allen Mi <allen.mi@yale.edu>
2018-12-10 17:11:58 -0500

    Remove debug info to test on derby first

--- src/compile/Compiler.scala
+++ src/compile/Compiler.scala
@@ -283,11 +283,13 @@ object Compiler {
       Register("r14"),
       Register("rbx")
     )
+    /*
     DUChainConstruct(optCFGFinal)
     // DUChainConstruct.testOutput()
     DUWebConstruct(DUChainConstruct.duChainSet)
     WebGraphColoring(DUWebConstruct.duWebSet, regVector)
     DUWebConstruct.testOutput
+    */
 
     Allocate(optCFGFinal)
 
